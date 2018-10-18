b9f934dc686087221102e498da330c389871101c
Gregory Austin <astgre002@namibia.cs.uct.ac.za>
2018-10-18 11:24:56 +0200

    Fixed for ubuntu

--- src/server.py
+++ src/server.py
@@ -1,24 +1,23 @@
 import socket
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 65433        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            try:
-                data = conn.recv(1024)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(0)
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+    try:
+	data = conn.recv(1024)
 
-                if not data:
-                    break
-                
-                print(data)
-                conn.sendall(data)
-                
-            except Exception:
-                break;
-            
+	if not data:
+	    break
+	
+	print(data)
+	conn.sendall(data)
+	
+    except Exception:
+	break;
+    
